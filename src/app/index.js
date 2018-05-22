7d727b8326e2693ba542b5ce838717c808c75530
Aidan <aidanoflann@gmail.com>
2018-05-22 22:35:06 +0900

    cleanup

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,10 @@
 const redis_db = require('../model/redis_db.js');
+const express = require('express');
 
 // this will be inside a Docker container, so use 0.0.0.0 instead of 127.0.0.1 loopback
 const hostname = '0.0.0.0';
 const port = 3000;
 
-const express = require('express');
 const app = express();
 app.set('view engine', 'pug');
 app.set('views', './src/views');
@@ -12,12 +12,7 @@ app.set('views', './src/views');
 app.get('/', (request, response) => {
     console.log('Request received');
     redis_db.getPlayers().then( (players) => {
-        console.log(players);
         response.render('index', {'currentlyLoggedInColour': players.CurrentlyLoggedInPlayer().colour});
     })
-
-    // console.log(players.CurrentlyLoggedInPlayer().colour);
-    // response.render('index', {"currentlyLoggedInColour": players.CurrentlyLoggedInPlayer().colour});
-    // console.log('Request complete');
 });
 app.listen(port, hostname, () => {console.log('Server started on port: ' + port)});
